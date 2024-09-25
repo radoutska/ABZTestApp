7da983a4a3cfb5dc6b77981b30c1d51fe3f070ba
Anna Radoutska <anna.radoutska@macys.com>
2024-09-25 08:40:23 +0300

    Initial Commit

---
+++ ABZTest/ABZTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABZTestApp.swift
+//  ABZTest
+//
+//  Created by Anna Radoutska on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ABZTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
